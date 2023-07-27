0b2de0ec9e496547373f824a79825c5e57c9a467
João Victor Novais <joao.dipaay@gmail.com>
2023-07-27 16:18:11 -0300

    feat: seed database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_USERS (id, name, email, phone, password) VALUES (1, 'Arthur Rodrigues', 'arthur@gmail.com', '11987898765', 'senha123')
+INSERT INTO TB_USERS (id, name, email, phone, password) VALUES (2, 'Pedro Henrique', 'pedro@gmail.com', '11983746525', 'senha456')
+INSERT INTO TB_USERS (id, name, email, phone, password) VALUES (3, 'Mateus Meira', 'mateus@gmail.com', '11987687632', 'senha789')
