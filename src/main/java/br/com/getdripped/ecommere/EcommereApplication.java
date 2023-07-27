136ec5968dfc2feba5b42f79eac14fdff67b3356
João Victor Novais <joao.dipaay@gmail.com>
2023-07-27 15:59:33 -0300

    Project created

---
+++ src/main/java/br/com/getdripped/ecommere/EcommereApplication.java
@@ -0,0 +1,13 @@
+package br.com.getdripped.ecommere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/getdripped/ecommere/EcommereApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.getdripped.ecommere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
